8d4019a572d238177aa33154bd2cc1f60103f142
xasano5o <abdurazakovxasan_1oo@mail.ru>
2022-05-23 23:44:39 +0500

    done

--- js/app.js
+++ js/app.js
@@ -1,83 +1,83 @@
 
-//Example
-var ary = ["A", "B", "C", "D", "E"];
+// //Example
+// var ary = ["A", "B", "C", "D", "E"];
 
-//length
-console.log(ary.length); //5
+// //length
+// console.log(ary.length); //5
 
-//push
-console.log(ary.push("F")); //6
+// //push
+// console.log(ary.push("F")); //6
 
-//pop
-console.log(ary.pop()); //"F"
+// //pop
+// console.log(ary.pop()); //"F"
 
-//unshift
-console.log(ary.unshift("F")); //6
+// //unshift
+// console.log(ary.unshift("F")); //6
 
-//shift
-console.log(ary.shift()); //"F"
+// //shift
+// console.log(ary.shift()); //"F"
 
-//splice(insert)
-console.log(ary.splice(3, 0, "F")); //[]
+// //splice(insert)
+// console.log(ary.splice(3, 0, "F")); //[]
 
-//splice(remove)
-console.log(ary.splice(3, 1)); //["F"]
+// //splice(remove)
+// console.log(ary.splice(3, 1)); //["F"]
 
-//indexOf
-console.log(ary.indexOf("D")); //3
+// //indexOf
+// console.log(ary.indexOf("D")); //3
 
-//lastIndexOf
-console.log(ary.lastIndexOf("D")); //3
+// //lastIndexOf
+// console.log(ary.lastIndexOf("D")); //3
 
-//join
-console.log(ary.join(":")); //"A:B:C:D:E"
+// //join
+// console.log(ary.join(":")); //"A:B:C:D:E"
 
-//reverse
-console.log(ary.reverse()); //["E", "D", "C", "B", "A"]
+// //reverse
+// console.log(ary.reverse()); //["E", "D", "C", "B", "A"]
 
-//sort
-console.log(ary.sort()); //["A", "B", "C", "D", "E"]
+// //sort
+// console.log(ary.sort()); //["A", "B", "C", "D", "E"]
 
-//some
-console.log(ary.some(function(item) {
-	return item === "C";
-})); //true
+// //some
+// console.log(ary.some(function(item) {
+// 	return item === "C";
+// })); //true
 
-//every
-console.log(ary.every(function(item) {
-	return item.length === 1;
-})); //true
+// //every
+// console.log(ary.every(function(item) {
+// 	return item.length === 1;
+// })); //true
 
-//forEach
-console.log(ary.forEach(function(item) {
-	return item;
-})); //undefined
+// //forEach
+// console.log(ary.forEach(function(item) {
+// 	return item;
+// })); //undefined
 
-//reduce
-console.log(ary.reduce(function(prevItem, item) {
-	return prevItem + item;
-})); //"ABCDE"
+// //reduce
+// console.log(ary.reduce(function(prevItem, item) {
+// 	return prevItem + item;
+// })); //"ABCDE"
 
-//reduceRight
-console.log(ary.reduceRight(function(prevItem, item) {
-	return prevItem + item;
-})); //"EDCBA"
+// //reduceRight
+// console.log(ary.reduceRight(function(prevItem, item) {
+// 	return prevItem + item;
+// })); //"EDCBA"
 
-//map
-console.log(ary.map(function(item) {
-	return item + item;
-})); //["AA", "BB", "CC", "DD", "EE"]
+// //map
+// console.log(ary.map(function(item) {
+// 	return item + item;
+// })); //["AA", "BB", "CC", "DD", "EE"]
 
-//filter
-console.log(ary.filter(function(item) {
-	return item > "C";
-})); //["D", "E"]
+// //filter
+// console.log(ary.filter(function(item) {
+// 	return item > "C";
+// })); //["D", "E"]
 
-//slice
-console.log(ary.slice(2, 4)); //["C", "D"]
+// //slice
+// console.log(ary.slice(2, 4)); //["C", "D"]
 
-//concat
-console.log(ary.concat(["G", "H"])); //["A", "B", "C", "D", "E", "G", "H"]
+// //concat
+// console.log(ary.concat(["G", "H"])); //["A", "B", "C", "D", "E", "G", "H"]
 
 
 // ------------------------------------------------------------------
@@ -106,11 +106,62 @@ console.log(ary.concat(["G", "H"])); //["A", "B", "C", "D", "E", "G", "H"]
 
 // console.log(person);
 
-// let user = {
+// const user = {
 //   name: "crystal",
-//   age: 30,
+//   age: 350,
 //   email: "crystal@thenetninja.co.uk",
 //   location: "berlin",
 //   blogs: ["why mac&cheese rules", "10 things to make with marmite"],
 // };
-// console.log(Object.keys(user));
+// user.blogs='7'
+// console.log(user);
+
+
+// var one =123
+// one='ygf'
+// var one=30
+// console.log(one);
+
+// let one =123
+// one='yfg'
+// console.log(one);
+
+// const one= 'hello world'
+// one ='welcome'
+// console.log(one);
+
+
+// {
+//     var one ='123'
+// }
+// console.log(one);
+
+
+
+
+// primative non primitiv
+// primitiv  1string  2number  3boolean 4 null 5 undifnd 6 bigInt 
+// non primativ  1arrey 2object 3 regex  
+// dtatype 1string  2number  3boolean 4 null 5 undifnd 6 bigInt 1arrey 2object 3 regex  
+
+// string 'hello world' "hello world" `hello world`
+// one=[]
+// obkect{}
+// console.log(one);
+
+
+// let one = 'hellojhksdhjshkjhjhkskjhsfjkh,uyuy'
+
+// console.log(one.toUpperCase());
+// console.log(one.toLocaleLowerCase());
+// console.log(one.lastIndexOf('l'));
+// console.log(one.slice(3,5));
+// console.log(one.substring(3,5));
+// console.log(one.replace('hello','xasan'));
+// console.log(one.concat(too));
+// console.log(one.trimEnd().split());
+// console.log(one.padEnd(21,'xasan'));
+// console.log(one.length);
+
+
+console.log(123);
